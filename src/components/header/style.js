b46206e64910d6cf7f18a67b687d0b1c2ac9a3d7
xyy547491937 <18210854820@163.com>
2019-06-25 23:52:15 +0800

    sxzdfvds afdsa

---
+++ src/components/header/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+export const HeaderWrapper=styled.div`
+	height:26px;
+	background:green;
+`
+export const Logo =styled.a`
+	height:58px;
+	position:absolute;
+	width:100px;
+	display:block;
+	background:#f00;
+`
+export const  Nav=styled.div`
+	width:960px;
+	
+
+` 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './style.js';
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
